70fb02b28e3cd0d78fc465059b526ab799c55b7c
kermitt PDX <bluefishbrowndirt@gmail.com>
2020-07-12 12:51:35 -0500

    added templated . webpage
    Added templated web page

--- app/main.py
+++ app/main.py
@@ -1,10 +1,19 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
 import events_router
 from db import metadata, database, engine
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
 
 metadata.create_all(engine)
 
 app = FastAPI(docs_url="/events_docs/", redoc_url=None)
+app.mount("/static", StaticFiles(directory="static"), name="static")
+templates = Jinja2Templates(directory="templates")
+
+@app.get("/items/{id}")
+async def read_item(request: Request, id: str):
+    return templates.TemplateResponse("item.html", {"request": request, "id": id})
+
 
 @app.on_event("startup")
 async def startup():
